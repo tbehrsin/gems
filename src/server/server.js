4de3b5043601d98ed91de4442d665196f96eb049
Tim Behrsin <tim@behrsin.com>
2015-12-14 22:51:10 +0000

    update buildpack

--- src/server/server.js
+++ src/server/server.js
@@ -10,3 +10,4 @@ app.use('/', express.static(path.resolve(__dirname, '../../dist')));
 app.listen(process.env.PORT || 3000, function() {
   console.info('Listening on ' + (process.env.PORT || 3000));
 });
+
